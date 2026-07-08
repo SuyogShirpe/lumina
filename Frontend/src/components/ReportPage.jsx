0c284092fdd5174f7d5e80e22fb34a6ce27df838
Suyog Shirpe <shirpesuyog@gmail.com>
2026-07-09 00:14:43 +0530

    updated Report page

--- Frontend/src/components/ReportPage.jsx
+++ Frontend/src/components/ReportPage.jsx
@@ -1,8 +1,6 @@
 export default function ReportPage() {
-
   const { categories } = useCategories();
 
-  
   const [formData, setFormData] = useState({
     categoryId: "",
     title: "",
@@ -13,27 +11,60 @@ export default function ReportPage() {
   });
 
   const [photos, setPhotos] = useState([]);
+  const [photoError, setPhotoError] = useState("");
+
+  const [userLocation, setUserLocation] = useState(null);
 
   const handlechange = (e) => {
     const { name, value } = e.target;
-     setFormData(prev => ({
-        ...prev,
-        [name]: value
-     }));
-  }
-
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
   const handlePhotoChange = (e) => {
     const files = Array.from(e.target.files);
-    setPhotos(files);
-  }
+    e.target.value = "";
+    
+    if (photos.length + files.length > 3) {
+      setPhotoError("You can upload a maximum of 3 photos.");
+      return;
+    }
 
+    setPhotoError("");
+
+    setPhotos((prev) => [
+      ...prev,
+      ...files,
+    ]);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     console.log(formData);
-  }
+  };
+
+  const handleUseCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const location = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        setUserLocation(userLocation);
+        setFormData((prev) => ({
+          ...prev,
+          latitude: location.lat,
+          longitude: location.lng,
+        }));
+      },
+      (error) => {
+        console.error("Error getting user location:", error);
+      },
+    );
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -50,10 +81,12 @@ export default function ReportPage() {
       ></textarea>
 
       <select
+        disabled={categories.length === 0}
         name="categoryId"
         value={formData.categoryId}
         onChange={handlechange}
       ></select>
+
       <input
         name="occurredAt"
         value={formData.occurredAt}
@@ -70,14 +103,21 @@ export default function ReportPage() {
         onChange={handlechange}
       />
 
+      <button onClick={handleUseCurrentLocation}>
+        Use my current location
+      </button>
+
       <input
         type="file"
         name="photos"
+        accept="image/*"
         multiple
         value={photos.photos}
         onChange={handlePhotoChange}
-        />
-        <button type="submit"  >Submit</button>
+      />
+      {photoError && <div className="text-danger">{photoError}</div>}
+
+      <button type="submit">Submit</button>
     </form>
   );
 }
